d93a7b284f19ec6e4132d14391275c628e3eac6a
unknown <qssshubham@gmail.com>
2019-10-06 11:43:20 +0530

    edit

--- firstflaskapp.py
+++ firstflaskapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)  # creating the Flask class object
 
 @app.route('/')  # decorator drfines the
 def home():
-    return "hello, this is our first flask website";
+    return "hello, this is our first flask website hello flask";
 
 
 if __name__ == '__main__':
